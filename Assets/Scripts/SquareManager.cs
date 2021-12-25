5d666e6dafee215f32c895ecd7bc8214862d1305
ariaenggar <aria.enggar@gmail.com>
2021-12-25 11:38:11 +0700

    simplify

--- Assets/Scripts/SquareManager.cs
+++ Assets/Scripts/SquareManager.cs
@@ -8,27 +8,18 @@ public class SquareManager : MonoBehaviour
     [SerializeField] private int minSquareCount = 10;
     [SerializeField] private int maxSareCount = 30;
 
-
     private void Start()
-    {
-        SpawnSquare();
-    }
-
-    private void SpawnSquare()
     {
         float squareSpawn = Random.Range(minSquareCount, maxSareCount);
         for(int i = 0; i < squareSpawn; i++)
         {
             GameObject newSquare = Instantiate(square, square.transform.position, Quaternion.identity);
-            newSquare.transform.position = GetRandomSpawnPos();
+            
+            float randomY = Random.Range(-3.5f, 3.5f);
+            float randomX = Random.Range(-7.8f, 7.8f);
+            
+            newSquare.transform.position = new Vector2(randomX, randomY);
         }
-    }
-
-    private Vector2 GetRandomSpawnPos()
-    {
-        float randomY = Random.Range(-3.5f, 3.5f);
-        float randomX = Random.Range(-7.8f, 7.8f);
-        return new Vector2(randomX, randomY);
     }
 
 }
